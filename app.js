7013f1ebe7df90d2042c351331be170ed4f23f2f
HYJ <teen14y@naver.com>
2019-05-31 17:17:15 +0900

    eject duplicated, blank name & save on localStorage

--- app.js
+++ app.js
@@ -13,6 +13,70 @@ document.addEventListener('DOMContentLoaded', () => {
   hideDiv.appendChild(hideCheckBox);
   mainDiv.insertBefore(hideDiv,ul);
 
+  //Local Storage 가 있는지 확인
+  function supportLocalStorage() {
+    return 'localStorage' in window || window[localStorage] !== null;
+  }
+  // Local Storage에 있는 값 읽기
+  function getInvitees() {
+    let inviteesList = localStorage.getItem('inviteesList');
+    if(inviteesList){
+      return JSON.parse(inviteesList);
+    } else {
+      return [];
+    }
+  }
+
+  // Local storage에 값 저장하기
+  function saveInvitees(str) {
+    let inviteesList = getInvitees();
+    if(!str || inviteesList.indexOf(str) > -1 ) {
+      return false;
+    }
+    inviteesList.push(str);
+    localStorage.setItem('inviteesList',JSON.stringify(inviteesList));
+  }
+
+  // Local storage에 값 수정하기
+  function editInvitees(element) {
+    function getEditedIndex(element) {
+      for(let i =0 ; i <element.parentNode.childNodes.length; i ++) {
+        if(element === element.parentNode.childNodes[i]){
+          return i;
+        }
+      }
+      console.log('Cannot figure out the index');
+    }
+    let inviteesList = getInvitees();
+    const elementText= element.firstElementChild.textContent;
+    const editedIndex = getEditedIndex(element);
+    inviteesList[editedIndex] = elementText;
+    localStorage.setItem('inviteesList',JSON.stringify(inviteesList));
+  }
+
+  // Local storage값 삭제하기
+  function removeInvitee(element) {
+    let inviteesList = getInvitees();
+    const elementText= element.firstElementChild.textContent;
+    const newInvitees = inviteesList.filter(invitee => invitee !== elementText)
+    localStorage.setItem('inviteesList',JSON.stringify(newInvitees));
+  }
+  // 윈도우가 실행이되면 바로 실행될 코드
+  window.onload = function() {
+    let inviteesList = getInvitees();
+    if(supportLocalStorage()){
+      inviteesList.forEach(function(text){
+        console.log(text);
+        ul.appendChild(createLI(text));
+      });
+
+    }
+  }
+  window.onunload = function() {
+    console.log('onunload');
+  }
+  // 윈도우가 reload 될때
+
   //Hide who haven't responded 체크박스 이벤트 핸들러
   hideCheckBox.addEventListener('change', (e) => {
     const isChecked = hideCheckBox.checked;
@@ -60,12 +124,17 @@ document.addEventListener('DOMContentLoaded', () => {
   //입력창 submit 누르면 createLI 함수 실행
   form.addEventListener('submit', (e)=>{
     e.preventDefault();// Submit 이벤트 발생시 리로드 되는 기본 설정이 실행되지 않게해준다.
-    const text = input.value;
-    input.value ='';
-    const li = createLI(text);
-    console.log(li);
-    ul.appendChild(li);
-
+    if(input.value == "") {
+      alert('Please type the name');
+    } else if(getInvitees().indexOf(input.value) > -1 ){
+      alert('He(she) is already on the list');
+    } else {
+      const text = input.value;
+      input.value ='';
+      const li = createLI(text);
+      ul.appendChild(li);
+      saveInvitees(text);
+    }
 
   });
 
@@ -88,11 +157,11 @@ document.addEventListener('DOMContentLoaded', () => {
     if(e.target.tagName === 'BUTTON'){
       const button = e.target;
       const buttonName = button.textContent;
-      console.log(buttonName);
       const listItem = button.parentNode;
       const actions = {
         remove: () => {
           listItem.parentNode.removeChild(listItem);
+          removeInvitee(listItem);
         },
         edit:() => {
           const span = listItem.firstElementChild;
@@ -110,6 +179,7 @@ document.addEventListener('DOMContentLoaded', () => {
           listItem.insertBefore(span,input);
           listItem.removeChild(input);
           button.textContent="edit";
+          editInvitees(listItem);
         }
       };
       actions[buttonName]();
